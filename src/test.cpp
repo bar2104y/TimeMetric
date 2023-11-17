04d0c309bb0e6337552a48a3915c766918981819
Yury Barichev <ybarichev@yandex.ru>
2023-11-17 17:57:15 +0300

    update example

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,23 @@
 #include "TimeMetric.h"
 
+#include <cmath>
+#include <iomanip>
+
 int main()
 {
-    TimeMetric t; 
+    TimeMetric t;
+
+    float y = 1;
+    uint64_t n = 100000000;
+
+    t.start();
+    for (uint64_t i=1; i<n; i++)
+    {
+        y = i*y/i;
+    } 
+    t.stop();
+
+    t.print();
+
+    std::cout<<std::fixed << std::setprecision(9)<<"After "<<n<<" iterations error is "<<std::fabs(y-1.0)<<std::endl;
 }
